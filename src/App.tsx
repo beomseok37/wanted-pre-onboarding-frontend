76a260f4fa74e038fa1f291eb451095aee7877e4
cotton <bmtosss@gmail.com>
2022-12-12 18:09:34 +0900

    style: global style 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Global } from '@emotion/react';
 
 import SignInPage from 'src/pages/signIn';
 import SignUpPage from 'src/pages/signUp';
 import TodoPage from 'src/pages/todo';
 
+import { globalStyle } from './globalStyle';
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Navigate to='signIn' />} />
-        <Route path='/signin' element={<SignInPage />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/todo' element={<TodoPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Fragment>
+      <Global styles={globalStyle} />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navigate to='signIn' />} />
+          <Route path='/signin' element={<SignInPage />} />
+          <Route path='/signup' element={<SignUpPage />} />
+          <Route path='/todo' element={<TodoPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Fragment>
   );
 }
 
---
+++ src/globalStyle.ts
@@ -0,0 +1,155 @@
+import { css } from '@emotion/react';
+
+const globalStyle = css`
+  html,
+  body,
+  div,
+  span,
+  applet,
+  object,
+  iframe,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  blockquote,
+  pre,
+  a,
+  abbr,
+  acronym,
+  address,
+  big,
+  cite,
+  code,
+  del,
+  dfn,
+  em,
+  img,
+  ins,
+  kbd,
+  q,
+  s,
+  samp,
+  small,
+  strike,
+  strong,
+  sub,
+  sup,
+  tt,
+  var,
+  b,
+  u,
+  i,
+  center,
+  dl,
+  dt,
+  dd,
+  ol,
+  ul,
+  li,
+  fieldset,
+  form,
+  label,
+  legend,
+  table,
+  caption,
+  tbody,
+  tfoot,
+  thead,
+  tr,
+  th,
+  td,
+  article,
+  aside,
+  canvas,
+  details,
+  embed,
+  figure,
+  figcaption,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  output,
+  ruby,
+  section,
+  summary,
+  time,
+  mark,
+  audio,
+  video,
+  button {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 0;
+    font: inherit;
+    vertical-align: baseline;
+    font-size: 1rem;
+    font-weight: bold;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article,
+  aside,
+  details,
+  figcaption,
+  figure,
+  footer,
+  header,
+  hgroup,
+  menu,
+  nav,
+  section {
+    display: block;
+  }
+  html {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+  }
+  body {
+    width: 100%;
+    height: 100%;
+    line-height: 1;
+    overflow-x: hidden;
+  }
+  ol,
+  ul {
+    list-style: none;
+  }
+  blockquote,
+  q {
+    quotes: none;
+  }
+  blockquote:before,
+  blockquote:after,
+  q:before,
+  q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  #__next {
+    height: 100%;
+  }
+
+  #root {
+    width: 100%;
+    height: 100%;
+  }
+
+  a {
+    text-decoration-line: none;
+    color: #000;
+  }
+`;
+
+export { globalStyle };
