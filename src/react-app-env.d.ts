96c2c4200f912cade77bbb413696db8676d1e91a
cotton <bmtosss@gmail.com>
2022-12-12 21:13:07 +0900

    chore: env type 추가

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,8 @@
+/* eslint-disable no-unused-vars */
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly REACT_APP_SERVER_URL: string;
+  }
+}
