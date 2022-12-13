53a8ae050d862091fefc8850af1b382ed4f69b82
cotton <bmtosss@gmail.com>
2022-12-13 11:44:17 +0900

    chore: Validator에 axios 추가

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,23 +1,35 @@
 import { VALID } from 'src/constants/valid';
 
-const emailValidator = (email: string) => {
-  if (!email) {
-    return VALID.EMPTY;
-  } else if (email.includes('@')) {
-    return VALID.INCLUDE_AT;
-  } else {
-    return VALID.NOT_INCLUDE_AT;
-  }
+const Validator = {
+  email(email: string) {
+    if (!email) {
+      return VALID.EMPTY;
+    } else if (email.includes('@')) {
+      return VALID.INCLUDE_AT;
+    } else {
+      return VALID.NOT_INCLUDE_AT;
+    }
+  },
+  password(password: string) {
+    if (!password) {
+      return VALID.EMPTY;
+    } else if (password.length >= 8) {
+      return VALID.EXCEEDING_STANDARD;
+    } else {
+      return VALID.BELOW_STANDARD;
+    }
+  },
+  axios(message: string) {
+    if (!message) {
+      return VALID.EMPTY;
+    } else if (message === '동일한 이메일이 이미 존재합니다.') {
+      return VALID.ALREADY_EXIST_EMAIL;
+    } else if (message === '해당 사용자가 존재하지 않습니다.') {
+      return VALID.NOT_EXIST_EMAIL;
+    } else {
+      return VALID.UNAUTHORIZED;
+    }
+  },
 };
 
-const passwordValidator = (password: string) => {
-  if (!password) {
-    return VALID.EMPTY;
-  } else if (password.length >= 8) {
-    return VALID.EXCEEDING_STANDARD;
-  } else {
-    return VALID.BELOW_STANDARD;
-  }
-};
-
-export { emailValidator, passwordValidator };
+export { Validator };
